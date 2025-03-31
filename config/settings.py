36de75e34d534caae5177e7b6c24af237bc280e7
Ali Rostami <AliRostami1253@gmail.com>
2025-03-31 12:59:14 +0330

    change config jwt in settings

--- config/settings.py
+++ config/settings.py
@@ -204,7 +204,7 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("jwt",),
+    "AUTH_HEADER_TYPES": ("JWT",),
     "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
 }
